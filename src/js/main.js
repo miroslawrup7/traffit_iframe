90b419bad89b64902d7cec609877282b251f89ba
Mirek Rup <miroslawrup7@gmail.com>
2023-11-02 13:06:42 +0100

    Changed iframe source address

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 const iframe = document.querySelector("#iframe");
 
 window.addEventListener('message', function(event) {
-    if (event.origin === 'http://localhost:3000') {
+    if (event.origin === 'https://miroslawrup7.github.io/traffit7_5/dist/') {
         iframe.height = event.data;
     }
 });
